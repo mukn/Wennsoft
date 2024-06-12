f28f063be51e0decf7ff701ad7674614af620dd3
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-06-12 16:02:43 -0400

    Update Z_Active_contracts.sql
    Updated with reference to Z_Active_customers.

--- Z_Active_contracts.sql
+++ Z_Active_contracts.sql
@@ -1,12 +1,14 @@
+-- Active contracts
 SELECT
 	-- Customer information
-	TRIM(CUSTNMBR) AS Customer_Code
+	TRIM(c.CUSTNMBR) AS Customer_Code
+	
 
 	-- Customer contact information
 	,NULL AS Customer_ContactName
 
 	-- Location information
-	,TRIM(ADRSCODE) AS Location_Code
+	,TRIM(c.ADRSCODE) AS Location_Code
 	,NULL AS Location_Name
 	,NULL AS Location_Address1
 	,NULL AS Location_Address2
@@ -19,21 +21,24 @@ SELECT
 
 
 	-- Contract information
-	,TRIM(Contract_Number) AS Contract_Number
+	,TRIM(c.Contract_Number) AS Contract_Number
 	,NULL AS Contract_StartDate
 	,NULL AS Contract_EndDate
 	,NULL AS Contract_VisitCnt
 	,NULL AS Contract_Value
 
 	-- Technician information
-	,TRIM(Technician_ID) AS Tech_ID
+	,TRIM(c.Technician_ID) AS Tech_ID
 	,NULL AS Tech_Name
 	,NULL AS Tech_Email
 
 	-- Sales rep information
-	,TRIM(SLPRSNID) AS SalesRep_Code
+	,TRIM(c.SLPRSNID) AS SalesRep_Code
 	,NULL AS SalesRep_Name
 	,NULL AS SalesRep_Email
-	--,*
+	,cust.*
 FROM
 	SV00500 AS c
+	LEFT JOIN
+	Z_Active_customers AS cust
+		ON TRIM(c.CUSTNMBR) = cust.Customer_Code
