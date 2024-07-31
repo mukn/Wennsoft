1119c91b1bd144d9625abbf07f69fba701178eed
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-07-31 14:36:30 -0400

    Create Z_Report_purchase_orders.sql
    Initial outline.

---
+++ Z_Report_purchase_orders.sql
@@ -0,0 +1,24 @@
+-- Z_purchase_orders
+-- This is a list of all purchase orders recorded in Wennsoft.
+
+-- Date ordered
+-- Description
+-- Division
+-- Expected delivery
+-- Job description
+-- Job number
+-- PO number
+-- PO method
+-- City
+-- Requested by
+-- Ship to name
+-- Status flag
+-- Summary description
+-- Vendor code
+-- Written by
+
+
+<--
+POP10100  Purchase order work
+POP10110  Purchase order line
+-->
