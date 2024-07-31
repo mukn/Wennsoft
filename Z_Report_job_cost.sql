cf5efdc88ce85b0c6d0ff6e75d67bdfb382ddc96
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-07-31 15:14:52 -0400

    Update Z_Report_job_cost.sql
    Pulling two queries together to merge later. Need to determine which columns are correct.

--- Z_Report_job_cost.sql
+++ Z_Report_job_cost.sql
@@ -8,7 +8,7 @@ SELECT
   ,j.PM_Number AS PM -- PM
 
   
--- Cost Type
+-- Cost Type (labor, material, subcontractor, equipment)
 -- Est Cost
 -- Est Hours
 -- Foreman
@@ -32,3 +32,37 @@ SELECT
 
 FROM
   Z_Active_jobs AS j
+
+
+
+
+-- Job costs by job
+SELECT
+	TRIM(co.WS_Job_Number) AS Job_Number	-- Job number
+	,CASE
+		WHEN co.Cost_Element = 1 THEN 'Labor'
+		WHEN co.Cost_Element = 2 THEN 'Materials'
+		WHEN co.Cost_Element = 3 THEN 'Equipment'
+		WHEN co.Cost_Element = 4 THEN 'Tools'
+		WHEN co.Cost_Element = 5 THEN 'Other'
+		WHEN co.Cost_Element = 6 THEN 'Subcontract'
+		WHEN co.Cost_Element = 7 THEN 'Burden'
+		WHEN co.Cost_Element = 8 THEN 'Contingency'
+	END AS Cost_Type						-- Cost type
+	,co.Cost_Code_Act_Cost_TTD AS Actual_Cost -- JTD cost
+	,co.Cost_Code_Estimated_Cst AS Estimated_Cost -- Estimated cost
+	,co.Cost_Code_Forecast_Cost AS Forecast_Cost -- Projected cost
+	,Actual_Units_TTD AS Actual_Hours -- JDT hours
+	,co.Cost_Code_Est_Unit AS Estimated_Hours_A -- Estimated hours
+	,co.Estimated_Amt_Units AS Estimated_Hours_B
+	,co.Forecasted_Units AS Forecast_Hours -- Projected hours
+	,'' AS DIV
+	--,co.*
+FROM
+	JC00701 AS co
+--WHERE co.Cost_Element = 1
+ORDER BY WS_Job_Number DESC
+
+
+
+--SELECT * FROM JC00601
