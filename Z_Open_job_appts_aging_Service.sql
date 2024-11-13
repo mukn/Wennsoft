c2f6f15e2e34cc13b3de13264cf0b5560592ab9c
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-11-13 10:39:51 -0500

    Update Z_Open_job_appts_aging_Service.sql
    Updated with aging buckets and additional fields.

--- Z_Open_job_appts_aging_Service.sql
+++ Z_Open_job_appts_aging_Service.sql
@@ -9,6 +9,18 @@ SELECT
 	,TRIM(j.WS_Job_Name) AS Job_Name
 	,TRIM(CUSTNMBR) AS Customer_Code
 	,TRIM(Job_Address_Code) AS Location_Code
+	,CAST((GETDATE() - a.Task_Date) AS int) AS Age
+	,CASE
+		WHEN CAST((GETDATE() - a.Task_Date) AS int) > 180
+			THEN 180
+		WHEN CAST((GETDATE() - a.Task_Date) AS int) > 90
+			THEN 90
+		WHEN CAST((GETDATE() - a.Task_Date) AS int) > 60
+			THEN 60
+		WHEN CAST((GETDATE() - a.Task_Date) AS int) > 45
+			THEN 45
+		ELSE 30
+	END AS Aging_Bucket
 	--,j.*
 	--,a.*
 FROM
@@ -21,4 +33,5 @@ WHERE
 	AND Appointment_Status <> 'COMPLETE'		-- Select only appointments not marked complete
 	AND (GETDATE() - a.Task_Date ) > 30			-- Select only appointments aged by 30 or more days
 ORDER BY
-	j.WS_Job_Number DESC, a.Appointment ASC
+	--j.WS_Job_Number DESC, a.Appointment ASC
+	CAST((GETDATE() - a.Task_Date) AS int) DESC
