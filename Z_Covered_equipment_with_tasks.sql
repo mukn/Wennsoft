e839f42c115ee00631c334296f635eb71e0aac0c
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-05-13 14:29:33 -0400

    Update Z_Covered_equipment_with_tasks.sql
    Removed closed contracts.

--- Z_Covered_equipment_with_tasks.sql
+++ Z_Covered_equipment_with_tasks.sql
@@ -1,18 +1,22 @@
 SELECT
-	TRIM(CUSTNMBR) AS Customer_code
-	,TRIM(adrscode) AS Location_code
-	,TRIM(Contract_Number) AS Contract_number
-	,TRIM(Equipment_ID) AS Equipment_name
-	,LEFT(Task_code, 2) AS Task_code
+	TRIM(c.CUSTNMBR) AS Customer_code
+	,TRIM(c.adrscode) AS Location_code
+	,TRIM(c.Contract_Number) AS Contract_number
+	,TRIM(c.Equipment_ID) AS Equipment_name
+	,LEFT(c.Task_code, 2) AS Task_code
 	--,TRIM(Task_Code) AS Task_ID
-	,Contract_Task_List_ID 
+	,c.Contract_Task_List_ID 
 
 FROM
-	SV00585
+	SV00585 AS c
+	LEFT OUTER JOIN
+	SV00501 AS h
+		ON c.Contract_Number = h.Contract_Number
 
 WHere 
 	Required = '1'
-	And Contract_Number in (SELECT Contract_Number FROM SV00500)
+	And c.Contract_Number in (SELECT Contract_Number FROM SV00500)
+	AND h.Contract_Number IS NULL
 	
-GROUP BY CUSTNMBR, adrscode, Contract_Number, Equipment_ID, LEFT(Task_code, 2), Contract_Task_List_ID
-ORDER BY Customer_code, Location_code, Contract_number, Equipment_ID
+GROUP BY c.CUSTNMBR, c.adrscode, c.Contract_Number, c.Equipment_ID, LEFT(Task_code, 2), c.Contract_Task_List_ID
+ORDER BY Customer_code, Location_code, c.Contract_number, c.Equipment_ID
