0e40305ef4b5db684ab3689a57e3bc383be84e67
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-05-20 13:58:24 -0400

    Create Remove-TimeBatch.ps1
    Initial commit.

---
+++ Scripts/Remove-TimeBatch.ps1
@@ -0,0 +1,15 @@
+<#
+This should be inserted into Connect-Wennsoft.ps1 to remove a given time batch.
+
+Removes a time batch based on name.
+#>
+
+param (
+  [Parameter(Mandatory=$True)]
+  [string]$batchName
+)
+
+$batchId = $batchName
+
+$qry = "DELETE FROM UPR10304 WHERE BACHNUMB = '$batchId'"
+return $qry
