f265c34d021d3aaadd4b5c8a7df76991d604486c
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-05-21 10:52:32 -0400

    Update Z_Purchase_orders_against_quoted_jobs.sql
    Updated with additional fields and logic to exclude unncecessary POs.

--- Z_Purchase_orders_against_quoted_jobs.sql
+++ Z_Purchase_orders_against_quoted_jobs.sql
@@ -1,10 +1,22 @@
-
+/**
+	This captures purchase orders opened against jobs, the vendor, date created, and total cost for analysis.
+*/
 SELECT 
 	--TOP 1000 
-	TRIM(PONUMBER) AS PO_number,
-	TRIM(JOBNUMBR) AS Job_number
-	--,* 
+	w.PO_number,
+	w.Job_number,
+	p.VENDORID AS Vendor_code,
+	p.VENDNAME AS Vendor_name,
+	p.CREATDDT AS Date_create,
+	w.Cost_total
+	--,w.*
+	--,p.*
 FROM
-	WS10101 
-WHERE 
-	JOBNUMBR <> ''
+	(SELECT TRIM(PONUMBER) AS PO_number, TRIM(JOBNUMBR) AS Job_number, Actual_Total_Cost AS Cost_total
+	FROM WS10101 GROUP BY PONUMBER,JOBNUMBR,Actual_Total_Cost) 
+		AS w
+	LEFT OUTER JOIN
+	POP10100 AS p
+		ON w.PO_number = TRIM(p.PONUMBER)
+WHERE
+	w.Job_number IN (SELECT TRIM(WS_Job_Number) FROM JC00102)
