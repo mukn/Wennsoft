9b78f14bc7caa4313c8ec7a008a70c1c94e8bdc8
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-06-09 15:24:32 -0400

    Update Remove-TimeBatch.ps1
    Updated to wrap into a function.

--- Scripts/Remove-TimeBatch.ps1
+++ Scripts/Remove-TimeBatch.ps1
@@ -1,15 +1,17 @@
-<#
-This should be inserted into Connect-Wennsoft.ps1 to remove a given time batch.
-
-Removes a time batch based on name.
-#>
-
-param (
-  [Parameter(Mandatory=$True)]
-  [string]$batchName
-)
-
-$batchId = $batchName
-
-$qry = "DELETE FROM UPR10304 WHERE BACHNUMB = '$batchId'"
-return $qry
+function Remove-TimeBatch {
+  <#
+  This should be inserted into Connect-Wennsoft.ps1 to remove a given time batch.
+  
+  Removes a time batch based on name.
+  #>
+  
+  param (
+    [Parameter(Mandatory=$True)]
+    [string]$batchName
+  )
+  
+  $batchId = $batchName
+  
+  $qry = "DELETE FROM UPR10304 WHERE BACHNUMB = '$batchId'"
+  return $qry
+}
