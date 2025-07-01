b816f3a7587af3b1a0f7cfe43b37b93f6ddf1a16
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-07-01 10:47:10 -0400

    Update Remove-TimeBatch.ps1
    Added some notes from a SQL script for later update.

--- Scripts/Remove-TimeBatch.ps1
+++ Scripts/Remove-TimeBatch.ps1
@@ -3,6 +3,14 @@ function Remove-TimeBatch {
   This should be inserted into Connect-Wennsoft.ps1 to remove a given time batch.
   
   Removes a time batch based on name.
+
+  Notes:
+  DECLARE @batchId varchar(30);
+  SET @batchId = '';
+  DELETE FROM UPR10304 WHERE BACHNUMB = @batchId;
+  GO
+
+  
   #>
   
   param (
