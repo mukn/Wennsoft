b399e4248d863d86ae7ab86c420d282cd31d3f78
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-08-01 09:05:58 -0400

    Create Remove_TimeBatch.sql
    Initial commit.

---
+++ Scripts/Remove_TimeBatch.sql
@@ -0,0 +1,10 @@
+/**      Remove_TimeBatch
+  
+  Removes a time batch based on name. Place the batch name into the variable.
+  
+  */
+  
+DECLARE @batchId varchar(30);
+  SET @batchId = '';
+  DELETE FROM DYNAMICS.dbo.UPR10304 WHERE BACHNUMB = @batchId;
+  GO
