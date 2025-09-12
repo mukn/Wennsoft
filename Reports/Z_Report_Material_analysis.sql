d51a781b47a216051cb42a6fbe77e7f46180bbb3
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-09-12 09:43:09 -0400

    Update Z_Report_Material_analysis.sql
    Completed first draft.

--- Reports/Z_Report_Material_analysis.sql
+++ Reports/Z_Report_Material_analysis.sql
@@ -1,7 +1,36 @@
+--ALTER VIEW Z_Report_Material_analysis AS
 SELECT
-	TRIM(u.WS_Job_Number) AS Job_number,
-	u.User_Define_4 AS Material_analysis
-
-	,u.*
+	TRIM(j.WS_Job_Number) AS Job_number,
+	--Job_name,
+	TRIM(j.WS_Job_Name) AS Job_name,
+	--Job_description,
+	j.User_Define_1 AS Job_description,
+	--Material_cost,
+	j.Act_Materials_Cost_TTD AS Material_cost,
+	--Material_estimate,
+	j.Est_Material_Cost AS Material_estimate,
+	--Variance,
+	CASE
+		WHEN j.Est_Material_Cost > 0 THEN (j.Act_Materials_Cost_TTD / j.Est_Material_Cost) 
+		ELSE 0
+	END AS Material_variance,
+	--Percent_complete,
+	j.Est_Pct_Complete_to_Date as Percent_complete,
+	--Division,
+	j.Divisions AS Division,
+	--Status
+	s.Status,
+	u.Reason AS Material_analysis,
+	u.Notes AS Analysis_notes
+	
+	--,j.*
+	--,u.*
 FROM
-	JC00107 AS u
+	JC00102 AS j
+	LEFT OUTER JOIN
+	cstb_JobUDF4Reasons AS u
+		ON j.WS_Job_Number = u.WS_Job_Number
+	LEFT OUTER JOIN
+	(SELECT TRIM(WS_Job_Number) AS Job_number, WS_Job_Type AS Status
+	FROM JC00107) AS s
+		ON j.WS_Job_Number = s.Job_number
