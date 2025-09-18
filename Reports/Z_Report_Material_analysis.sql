ae56dbc5f6959f7ea92ed80e32c4ca9ff488beac
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-09-18 13:46:53 -0400

    Update Z_Report_Material_analysis.sql
    Modified the source table.

--- Reports/Z_Report_Material_analysis.sql
+++ Reports/Z_Report_Material_analysis.sql
@@ -1,36 +1,33 @@
 --ALTER VIEW Z_Report_Material_analysis AS
 SELECT
-	TRIM(j.WS_Job_Number) AS Job_number,
-	--Job_name,
-	TRIM(j.WS_Job_Name) AS Job_name,
-	--Job_description,
+	--Status
+	j.Status,
+	--Job information
+	TRIM(j.[Job Number]) AS Job_number,
+	TRIM(j.[Job Name]) AS Job_name,
 	j.User_Define_1 AS Job_description,
 	--Material_cost,
-	j.Act_Materials_Cost_TTD AS Material_cost,
+	j.[Act Material] AS Material_cost,
 	--Material_estimate,
-	j.Est_Material_Cost AS Material_estimate,
+	j.[Est Material] AS Material_estimate,
 	--Variance,
 	CASE
-		WHEN j.Est_Material_Cost > 0 THEN (j.Act_Materials_Cost_TTD / j.Est_Material_Cost) 
+		WHEN j.[Est Material] > 0 THEN (j.[Act Material] / j.[Est Material]) 
 		ELSE 0
-	END AS Material_variance,
+	END AS Material_variance_pct,
+	(j.[Act Material] - j.[Est Material]) AS Material_variance_act,
 	--Percent_complete,
-	j.Est_Pct_Complete_to_Date as Percent_complete,
+	j.[Percent Complete] as Percent_complete,
 	--Division,
-	j.Divisions AS Division,
-	--Status
-	s.Status,
+	j.Division AS Division,
+	--Material analysis
 	u.Reason AS Material_analysis,
 	u.Notes AS Analysis_notes
 	
 	--,j.*
 	--,u.*
 FROM
-	JC00102 AS j
+	csvw_All_Jobs_Masterv2 AS j
 	LEFT OUTER JOIN
 	cstb_JobUDF4Reasons AS u
-		ON j.WS_Job_Number = u.WS_Job_Number
-	LEFT OUTER JOIN
-	(SELECT TRIM(WS_Job_Number) AS Job_number, WS_Job_Type AS Status
-	FROM JC00107) AS s
-		ON j.WS_Job_Number = s.Job_number
+		ON j.[Job Number] = u.WS_Job_Number
