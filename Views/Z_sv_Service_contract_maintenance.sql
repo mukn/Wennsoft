cc29e931328eb999d18fcf7ec509ed75dd8a2ccc
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-10-15 14:21:07 -0400

    Update Z_sv_Service_contract_maintenance.sql
    Updated a few fields.

--- Views/Z_sv_Service_contract_maintenance.sql
+++ Views/Z_sv_Service_contract_maintenance.sql
@@ -1,3 +1,4 @@
+
 /**		Z_sv_Service_contract_maintenance
 	
 	This is a collection of top level financial data for 
@@ -13,21 +14,29 @@ SELECT
 	,TRIM(c.ADRSCODE) AS Location_code
 	,TRIM(c.Contract_Number) AS Contract_number
 	,c.Contract_Description
-	,c.Divisions
+	--,CASE
+	--	WHEN c.Divisions LIKE '%HVAC%' THEN 'Service'
+	--	WHEN c.Divisions LIKE '%SP%' THEN 'Special Projects'
+	--	ELSE ''
+	--END AS Division
 	,c.Contract_Amount
 	,CASE
 		WHEN LOWER(c.User_Define_1a) LIKE 'n%' THEN 'No'
 		WHEN LOWER(c.User_Define_1a) LIKE 'y%' THEN 'Yes'
 		ELSE ''
 	END AS MaterialsIncl_Flag
-	,c.WSContractRenewalValue
 	,c.Bill_Freq
+	,CASE
+		WHEN c.WSContractRenewalValue <> c.Contract_Amount THEN c.WSContractRenewalValue
+	END AS Contract_renewal
 	,c.Amount_Billed
 	,c.Annual_Contract_Value
 	,CAST(c.Contract_Start_Date AS date) AS Date_start
 	,CAST(c.Contract_Expiration_Date AS date) AS Date_end
 	-- Forecast original values
 	,c.Forecast_Original_Labor
+	,((c.Forecast_Orig_Labor1_Hrs + c.Forecast_Orig_Labor3_Hrs + c.Forecast_Orig_Labor4_Hrs + c.Forecast_Orig_Labor5_Hrs) / 100.0) 
+		AS Forecast_Original_Hours
 	,'Not available' AS Forecast_Original_Material
 	,c.Forecast_Original_Equip
 	,c.Forecast_Original_Subs
@@ -35,6 +44,8 @@ SELECT
 	,'Not available' AS Forecast_Original_Total
 	-- Forecast values
 	,c.Forecast_Labor
+	,((c.Forecast_Labor_1_Hours + c.Forecast_Labor_2_Hours + c.Forecast_Labor_3_Hours + c.Forecast_Labor_4_Hours + c.Forecast_Labor_5_Hours) / 100.0)
+		AS Forecast_Hours
 	,c.Forecast_Material
 	,c.Forecast_Equipment
 	,c.Forecast_Subs
@@ -42,6 +53,8 @@ SELECT
 	,c.Forecast_Total_Cost
 	-- Estimate values
 	,c.Estimate_Labor
+	,((c.Estimate_Labor_1_Hours + c.Estimate_Labor_2_Hours + c.Estimate_Labor_3_Hours + c.Estimate_Labor_4_Hours + c.Estimate_Labor_5_Hours) / 100)
+		AS Estimate_Hours
 	,c.Estimate_Material
 	,c.Estimate_Equipment
 	,c.Estimate_Subs
@@ -49,6 +62,8 @@ SELECT
 	,c.Estimate_Total_Cost
 	-- Actual values
 	,c.Actual_Labor
+	,c.Actual_Hours / 100.0
+		AS Actual_hours
 	,c.Actual_Material
 	,c.Actual_Equipment
 	,c.Actual_Subs
@@ -73,7 +88,6 @@ SELECT
 	,c.YTD_Revenue_Recognized
 	,CASE
 		WHEN c.Cancel_Box > 0 THEN 'Cancelled'
-		ELSE ''
 	END AS Cancelled
 	,CASE
 		WHEN c.HOLD > 0 THEN 'Hold'
