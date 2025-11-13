a3a205a71d0a4553da9b17a4f9887446b0cfdf3b
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-11-13 10:42:24 -0500

    Update Z_All_jobs_udfs.sql
    Updated with cleaned fields.

--- Views/Z_All_jobs_udfs.sql
+++ Views/Z_All_jobs_udfs.sql
@@ -1,16 +1,22 @@
 --ALTER VIEW Z_All_jobs_udfs AS
 -- UDF field values
 SELECT 
-	u.WS_Job_Number AS Job_number
-	,u.WS_Manager_ID_2 AS Superintendent
-	,u.WS_Engineer AS Engineer
-	,u.User_Define_1 AS Est_hoursOT
-	,u.User_Define_2 AS Est_manpower
-	,u.User_Define_3 AS Foreman
-	,u.User_Define_4 AS Projected_hours
-	,u.USRDAT01 AS Date_Salesforce
-	,u.User_Defined_CB_1 AS Permit_reqd
-	,u.User_Defined_CB_2 AS Flag_priority
+	TRIM(u.WS_Job_Number) AS Job_number
+	,CAST(u.WS_Manager_ID_2 AS int) AS Superintendent
+	,CAST(u.WS_Engineer AS int) AS Engineer
+	,CAST(u.User_Define_1 AS int) AS Est_hoursOT
+	,CAST(u.User_Define_2 AS int) AS Est_manpower
+	,CAST(u.User_Define_3 AS int) AS Foreman
+	,CAST(u.User_Define_4 AS int) AS Projected_hours
+	,CAST(u.USRDAT01 AS date) AS Date_Salesforce
+	,CASE
+		WHEN u.User_Defined_CB_1 = 1 THEN 'True'
+		ELSE NULL
+	END AS Permit_reqd
+	,CASE
+		WHEN u.User_Defined_CB_2 = 1 THEN 'Priority'
+		ELSE NULL
+	END AS Flag_priority
 	--,u.*
 
 FROM JC00107 AS u		--WHERE WS_Job_Number = '24040940ET'
