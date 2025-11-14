136a413f695ea4e95c08e0a2f023fd676eeb11a8
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-11-14 11:15:54 -0500

    Update Z_All_jobs_udfs.sql
    Updated formatting.

--- Views/Z_All_jobs_udfs.sql
+++ Views/Z_All_jobs_udfs.sql
@@ -2,13 +2,34 @@
 -- UDF field values
 SELECT 
 	TRIM(u.WS_Job_Number) AS Job_number
-	,CAST(u.WS_Manager_ID_2 AS int) AS Superintendent
-	,CAST(u.WS_Engineer AS int) AS Engineer
-	,CAST(u.User_Define_1 AS int) AS Est_hoursOT
-	,CAST(u.User_Define_2 AS int) AS Est_manpower
-	,CAST(u.User_Define_3 AS int) AS Foreman
-	,CAST(u.User_Define_4 AS int) AS Projected_hours
-	,CAST(u.USRDAT01 AS date) AS Date_Salesforce
+	,CASE
+		WHEN u.WS_Manager_ID_2 <> '' THEN CAST(u.WS_Manager_ID_2 AS int)
+		ELSE NULL
+	END AS Superintendent
+	,CASE
+		WHEN u.WS_Engineer <> '' THEN CAST(u.WS_Engineer AS int)
+		ELSE NULL
+	END AS Engineer
+	,CASE
+		WHEN u.User_Define_1 <> '' THEN CAST(u.User_Define_1 AS int)
+		ELSE NULL
+	END AS Est_hoursOT
+	,CASE
+		WHEN u.User_Define_2 <> '' THEN CAST(u.User_Define_2 AS int)
+		ELSE NULL
+	END AS Est_manpower
+	,CASE
+		WHEN u.User_Define_3 <> '' THEN CAST(u.User_Define_3 AS int)
+		ELSE NULL
+	END AS Foreman
+	,CASE
+		WHEN u.User_Define_4 <> '' THEN CAST(u.User_Define_4 AS int)
+		ELSE NULL
+	END AS Projected_hours
+	,CASE
+		WHEN u.USRDAT01 <> '' THEN CAST(u.USRDAT01 AS date)
+		ELSE NULL
+	END AS Date_Salesforce
 	,CASE
 		WHEN u.User_Defined_CB_1 = 1 THEN 'True'
 		ELSE NULL
