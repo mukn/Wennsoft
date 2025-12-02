86c6eebf0fa6c0068c950a031e1daa84f63589cf
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2025-12-02 13:06:52 -0500

    Update Z_view_contract_scheduled_actual_hours.sql
    Updated to sum the actual hours as recorded in payroll.

--- Reports/Z_view_contract_scheduled_actual_hours.sql
+++ Reports/Z_view_contract_scheduled_actual_hours.sql
@@ -9,34 +9,24 @@
 SELECT
     TRIM(t.Contract_Number) AS Contract_number,
     CAST(t.Original_Schedule_Date AS DATE) AS [Visit Date],
-    TRIM(t.Service_Call_ID) AS Work_number,
+    --TRIM(t.Service_Call_ID) AS Work_number,
+	s.Work_Number,
     CASE 
         WHEN (t.Estimate_Hours > 0) THEN (t.Estimate_Hours / 100.0)
         ELSE 0 
     END AS [Budget Hours],
-    a_1.Hours_actual AS Appt_hours
-FROM dbo.SV00585 AS t WITH (NOLOCK)
-LEFT OUTER JOIN (
-    SELECT 
-        TRIM(Service_Call_ID) AS Work_number,
-        TRIM(WS_Job_Number) AS Job_number,
-        TRIM(Appointment) AS Appt_number,
-        TRIM(Appointment_Description) AS Appt_description,
-        TRIM(Appointment_Status) AS Appt_status,
-        CAST(Task_Date AS DATE) AS Date_ofWork,
-        CAST(Completion_Date AS DATE) AS Date_complete,
-        Estimate_Hours / 100.0 AS Hours_estimate,
-        Actual_Hours / 100.0 AS Hours_actual,
-        DEX_ROW_ID
-    FROM dbo.SV00301 AS a WITH (NOLOCK)
-    WHERE TRIM(Service_Call_ID) <> ''
-) AS a_1 ON TRIM(t.Service_Call_ID) = a_1.Work_number
-LEFT OUTER JOIN (
-    SELECT 
-        TRIM(Service_Call_ID) AS Work_number,
-        SUM(Actual_Hours / 100.0) AS Hours_actual
-    FROM dbo.SV00301 AS a WITH (NOLOCK)
-    WHERE TRIM(Service_Call_ID) <> ''
-    GROUP BY Service_Call_ID
-) AS s ON a_1.Work_number = s.Work_number
-WHERE t.Equipment_ID = 'HOURS';
+    h.Hours_actual AS Appt_hours
+FROM
+	(SELECT 
+		TRIM(Service_Call_ID) AS Work_number
+		FROM dbo.SV00301 AS a WITH (NOLOCK)
+	WHERE TRIM(Service_Call_ID) <> ''
+	GROUP BY Service_Call_ID
+		) AS s
+	LEFT OUTER JOIN
+		SV00585 AS t ON TRIM(t.Service_Call_ID) = s.Work_number
+	LEFT OUTER JOIN
+		(SELECT TRIM(WS_Job_Number) AS Work_number,SUM(TRXHRUNT / 100.) AS Hours_actual--,'',*
+			FROM WS30702 GROUP BY WS_Job_Number
+		) AS h ON TRIM(t.Service_Call_ID) = h.Work_number
+WHERE t.Equipment_ID = 'HOURS'
