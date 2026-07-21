e47c6893192e423b6ed054ba6310b8e76b4564e9
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2026-07-21 13:44:34 -0400

    Update Z_Service_call_metadata.sql
    Added Contract_flag field.

--- Views/Z_Service_call_metadata.sql
+++ Views/Z_Service_call_metadata.sql
@@ -1,11 +1,15 @@
 /**		-- Z_Service_call_metadata
 
 */
---CREATE VIEW Z_Service_call_metadata AS
+--ALTER VIEW Z_Service_call_metadata AS
 SELECT
 	TRIM(s.Service_Call_ID) AS Work_number
 	,TRIM(s.ADRSCODE) AS Location_code
 	,TRIM(s.CUSTNMBR) AS Customer_code
+	,CASE
+		WHEN s.Contract_Number <> '' THEN 'True'
+		ELSE 'False'
+	END as Contract_flag
 	,TRIM(s.Contract_Number) AS Contract_code
 	,TRIM(s.Service_Description) AS Work_description
 	,TRIM(s.Technician_ID) AS Technician_primary
